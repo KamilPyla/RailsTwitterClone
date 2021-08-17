99771b2c444ec4b632d8f1c9607a5383562477e1
Kamil Pyla <kamilpyla@gmail.com>
2021-08-17 16:23:01 +0200

    Create dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,32 +1,59 @@
 ActiveAdmin.register_page "Dashboard" do
+  # This file needs to be refactor!
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
+    # section "Reported Posts" do
+    #   table_for Micropost.reported do |post|
+    #     post.column('Post id') { |p| p.id }
+    #     post.column('Author') do |p|
+    #       author = User.find_by(id: p.user_id) 
+    #       link_to author.name, admin_user_path(author)
     #     end
     #   end
     # end
+
+    panel 'Show all reported posts' do
+      link_to('Show all', admin_microposts_path(q: { reported_eq: true }))
+    end
+    panel 'Reported Posts' do
+      columns do
+        column { b 'Post id' }
+        column { b 'Author' }
+        column { b 'Content' }
+        column { b 'Action' }
+        column { b 'Number of reports' }
+        column { b 'Comments' }
+      end
+      Micropost.reported.each do |post|
+        author = User.find_by(id: post.user_id)
+        columns do
+          column do 
+            'Micropost id ' + post.id.to_s
+          end
+          column do
+            link_to author.name, admin_user_path(author)
+          end
+          column do
+            i post.content
+          end
+          column do
+            link_to('Show post', admin_micropost_path(post)) + ' | ' + \
+            link_to('Show comments', admin_reported_comments_path(q: { micropost_id_eq: post.id }))
+          end
+          column do
+            span post.reported_comments.size.to_s
+          end
+          column do
+            post.reported_comments.each do |comment|
+              ul link_to comment.content, admin_reported_comment_path(comment)
+            end
+          end
+        end
+
+      end
+    end
+
   end # content
 end
