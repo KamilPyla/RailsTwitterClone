5a8b1aed959c9ce677cb3e63b6d1d9a9aecb2acd
Kamil Pyla <kamilpyla@gmail.com>
2021-08-20 13:45:17 +0200

    Override destroy method for categories.

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register Category do
   permit_params :name_en, :name_pl, :name_it, :name_cn
+  
+  controller do
+    def destroy
+      category = Category.find(params[:id])
+      category.microposts.each { |mpost| mpost.update(category_id: nil) }
+      category.destroy
+      flash[:success] = 'Category has been deleted.'
+      redirect_to admin_categories_path
+    end
+  end
 
 end
