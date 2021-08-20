81c922b1b16797fba6865d5abb55cab27a30b2af
Kamil Pyla <kamilpyla@gmail.com>
2021-08-20 13:53:33 +0200

    Add dependenses between user/micropost and reported_comments

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,7 +6,7 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate :picture_size
-  has_many :reported_comments
+  has_many :reported_comments, dependent: :destroy
 
   scope :reported, ->{ where(reported: true) }
   scope :blocked, ->{ where(blocked: true) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  has_many :reported_comments
+  has_many :reported_comments, dependent: :destroy
 
   attr_accessor :remember_token
 
